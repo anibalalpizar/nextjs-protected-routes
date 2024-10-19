946b8fe239a851075db9a765f41be8e7767b8356
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-19 00:56:22 -0600

    Add logout functionality and update profile API endpoint in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,15 +2,23 @@
 
 import axios from "axios";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 function DashboardPage() {
   const [user, setUser] = useState({
     email: "",
   });
+  const router = useRouter();
 
   const getProfile = async () => {
-    const res = await axios.get("/api/auth");
+    const res = await axios.get("/api/auth/profile");
     setUser(res.data);
+  };
+
+  const logout = async () => {
+    await axios.post("/api/auth/logout");
+    setUser({ email: "" });
+    router.push("/login");
   };
 
   return (
@@ -18,6 +26,7 @@ function DashboardPage() {
       <h1>Dashboard</h1>
       <pre>{JSON.stringify(user)}</pre>
       <button onClick={() => getProfile()}>Get Profile</button>
+      <button onClick={() => logout()}>Logout</button>
     </div>
   );
 }
