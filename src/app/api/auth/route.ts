fdfc12b145646e8dc19b0dc13b105a977786b0b6
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-18 21:36:05 -0600

    Refactor authentication route to use named imports for jwt and add GET method for token verification

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,12 +1,13 @@
-import jwt from "jsonwebtoken";
+import { verify, sign } from "jsonwebtoken";
 import { NextResponse } from "next/server";
 import { serialize } from "cookie";
+import type { UserPayload } from "@/types/types";
 
 export async function POST(req: Request) {
   const { email, password } = await req.json();
 
   if (email === "admin@admin.com" && password === "admin") {
-    const token = jwt.sign(
+    const token = sign(
       {
         exp: Math.floor(Date.now() / 1000) + 60 * 60,
         email,
@@ -31,5 +32,42 @@ export async function POST(req: Request) {
     return response;
   } else {
     return NextResponse.json("Invalid credentials", { status: 401 });
+  }
+}
+
+export async function GET(req: Request) {
+  // plain text cookie header
+  const cookieHeader = req.headers.get("cookie");
+
+  if (!cookieHeader)
+    return NextResponse.json(
+      { message: "No cookies present" },
+      { status: 401 }
+    );
+
+  // convert plain text cookie header to object
+  const cookies = Object.fromEntries(
+    cookieHeader.split(";").map((cookie) => cookie.trim().split("="))
+  );
+
+  const token = cookies["token-auth"];
+
+  if (!token)
+    return NextResponse.json({ message: "Token not found" }, { status: 401 });
+
+  try {
+    const user = verify(token, "secret") as UserPayload;
+    return NextResponse.json({ email: user.email }, { status: 200 });
+  } catch (e) {
+    if (e instanceof Error)
+      return NextResponse.json(
+        { message: `Invalid token: ${e.message}` },
+        { status: 401 }
+      );
+    else
+      return NextResponse.json(
+        { message: "Unknown error occurred" },
+        { status: 500 }
+      );
   }
 }
